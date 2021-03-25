4abe3d62f9f75d98581192a34b4d4938e53d4348
Linda Anelli <anellilinda1@gmail..com>
2021-03-25 16:00:12 +0100

    Selenium improved

--- cit.py
+++ cit.py
@@ -2,6 +2,14 @@ from selenium import webdriver
 import scrapy
 from scrapy.http import Request
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+class CitationItem(scrapy.Field):
+    Title = scrapy.Field()
+    Format = scrapy.Field
 
 
 class PButton(scrapy.Spider):
@@ -16,15 +24,19 @@ class PButton(scrapy.Spider):
 
         driver = webdriver.Chrome()
         driver.get(response.url)
-        sel = scrapy.Selector(text=driver.page_source)
         for x in driver.find_elements_by_css_selector("a.gs_or_cit.gs_nph"):
-            x.click()
             WebDriverWait(driver, 10)
+            x.click()
+            for name in response.css('div#gs_citt'):
+                name_title = name.css("div.gs_citr::text").extract()
+                name_format = name.css("th.gs_cith::text").extract()
 
-            links = sel.css("div.gs_md_bdy")
-            for link in links:
-                yield {
-                    "Title": link.css("div.gs_citr::text").extract()
-                }
+                item = CitationItem()
+                item["Title"] = name_title
+                item["Format"] = name_format
+                yield item
+            close = driver.find_element_by_css_selector("span.gs_ico")
+            driver.implicitly_wait(10)
+            ActionChains(driver).move_to_element(close).click(close).perform()
 
         driver.quit()
