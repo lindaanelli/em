2f232db5eebab65a068d1dc4044b66b12e950e3c
Linda Anelli <anellilinda1@gmail..com>
2021-04-05 08:56:10 +0200

    BibTeX finished

--- cit.py
+++ cit.py
@@ -9,9 +9,10 @@ link_to_click = []
 text_list = []
 temp = []
 bib = []
-final = []
+final_bib = []
 start = 0
 i = 0
+
 
 webdriver.Chrome()
 driver = webdriver.Chrome()
@@ -20,10 +21,15 @@ driver = webdriver.Chrome()
 # options.add_argument('--headless')
 # driver = webdriver.Chrome(options=options)
 
-while i < 10:
+while True:
 
-    url = "https://scholar.google.com/scholar?oi=bibs&hl=en&cites=10972803237645083033&start={}".format(start)
+    url = "https://scholar.google.it/scholar?oi=bibs&hl=it&cites=9931762518269776070&start={}".format(start)
     driver.get(url)
+
+    control = driver.find_element(By.ID, "gs_res_ccl_mid").text
+    control = str(control)
+    if not control:
+        break
 
     for x in driver.find_elements_by_css_selector("a.gs_or_cit.gs_nph"):
         WebDriverWait(driver, 10)
@@ -34,10 +40,15 @@ while i < 10:
 
         for element in search:
             title = element.find_elements_by_css_selector('div.gs_citr')
+            types = element.find_elements_by_css_selector("th.gs_cith")
 
             for t in title:
                 t2 = t.text
-                temp.append(str(t2))
+                temp.append([str(t2)])
+
+            for typ in types:
+                typ2 = typ.text
+                temp.append([str(typ2)])
             text_list.append(temp.copy())
             temp.clear()
 
@@ -51,6 +62,8 @@ while i < 10:
     start = start + 10
     i = 1 + i
 
+
+
 for link in link_to_click:
     driver.get(link)
     content = driver.find_element_by_css_selector("body")
@@ -60,11 +73,12 @@ for link in link_to_click:
 for pub in bib:
     for element in pub:
         stripped_line = element.replace('\n', '')
-        final.append([stripped_line])
+        final_bib.append([stripped_line])
 
 
 driver.quit()
-print(bib)
+print(text_list)
+print(final_bib)
 
 
 # TODO: go to page 2
